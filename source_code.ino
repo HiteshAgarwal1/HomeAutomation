e694bc9ae9cd438bb10fc776083fa8a8c437b21a
Hitesh Agarwal <42700049+HiteshAgarwal1@users.noreply.github.com>
2019-11-03 09:14:32 +0530

    Add files via upload

---
+++ source_code.ino
@@ -0,0 +1,37 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "d5c737c7d8554ae8aaa7ec95a8417414";
+
+char ssid[] = "Hitesh Agarwal";
+char pass[] = "99885566";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  int val=Serial.read();
+    Serial.println(val);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D3,OUTPUT);
+  pinMode(D4,OUTPUT);
+  
+    pinMode(D1,LOW);
+    pinMode(D2,LOW);
+    pinMode(D3,LOW);
+    pinMode(D4,LOW);
+}
+
+void loop()
+{
+  
+  
+  Blynk.run();
+  
+
+  
+}
+
